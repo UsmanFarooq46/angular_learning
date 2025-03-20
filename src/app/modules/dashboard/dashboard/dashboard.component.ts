531e369ca387e1dc5a581fce68d2873926f02912
UsmanFarooq46 <usman4609903@gmail.com>
2025-03-20 09:13:05 +0500

    dashboard design

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { DashboardLayoutComponent } from '../../../../app-shared-ui/dashboard-layout/dashboard-layout.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [DashboardLayoutComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
